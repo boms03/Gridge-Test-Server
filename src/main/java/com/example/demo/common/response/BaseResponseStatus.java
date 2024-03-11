3cb50f8e15328addf56a272be7db7503a68db8f4
boms03 <beomsuyoo@gmail.com>
2024-03-11 17:44:56 +0900

    [DOCS] 카카오 로그인 오류 enum 추가 완료

--- src/main/java/com/example/demo/common/response/BaseResponseStatus.java
+++ src/main/java/com/example/demo/common/response/BaseResponseStatus.java
@@ -42,6 +42,7 @@ public enum BaseResponseStatus {
     INVALID_USER_JWT(false,HttpStatus.FORBIDDEN.value(),"권한이 없는 유저의 접근입니다."),
     NOT_FIND_USER(false,HttpStatus.NOT_FOUND.value(),"일치하는 유저가 없습니다."),
     INVALID_OAUTH_TYPE(false, HttpStatus.BAD_REQUEST.value(), "알 수 없는 소셜 로그인 형식입니다."),
+    KAKAO_ERROR(false, HttpStatus.BAD_REQUEST.value(), "카카오 토큰 오류입니다."),
 
 
 
