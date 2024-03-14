c2a2136c1509ce9d54cea4e1e43830b4ea3bee31
boms03 <beomsuyoo@gmail.com>
2024-03-14 13:02:08 +0900

    [FEAT] 유저 약관 레포지토리 구현 완료

---
+++ src/main/java/com/example/demo/src/mapping/userAgree/UserAgreeRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.src.mapping.userAgree;
+
+import com.example.demo.src.mapping.userAgree.entity.UserAgree;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAgreeRepository extends JpaRepository<UserAgree,Long> {
+}
