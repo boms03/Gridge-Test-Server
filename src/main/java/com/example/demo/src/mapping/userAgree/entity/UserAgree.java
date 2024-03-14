cc4838bc58c3e4a9f8d1da887cab5c52d81ce431
boms03 <beomsuyoo@gmail.com>
2024-03-14 13:02:51 +0900

    [FEAT] 유저 이용약관 매핑 엔티티 추가 완료

---
+++ src/main/java/com/example/demo/src/mapping/userAgree/entity/UserAgree.java
@@ -0,0 +1,35 @@
+package com.example.demo.src.mapping.userAgree.entity;
+
+
+import com.example.demo.common.entity.BaseEntity;
+import com.example.demo.src.terms.entity.Terms;
+import com.example.demo.src.user.entity.User;
+import lombok.*;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = false)
+@Getter
+@Entity
+@Table(name = "USER_AGREE")
+public class UserAgree extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "terms_id", nullable = false)
+    private Terms terms;
+
+    @Builder
+    public UserAgree(User user, Terms terms){
+        this.user = user;
+        this.terms = terms;
+    }
+}
