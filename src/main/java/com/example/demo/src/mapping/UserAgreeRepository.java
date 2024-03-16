0a951800bc2d7c32eeae41b6c7c7bd84b3c18eb9
boms03 <beomsuyoo@gmail.com>
2024-03-16 18:48:59 +0900

    [Remove] 이동된 유저 이용약관 관련 파일들 삭제 완료

--- src/main/java/com/example/demo/src/mapping/UserAgreeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.src.mapping;
-
-import com.example.demo.src.mapping.userAgree.UserAgree;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAgreeRepository extends JpaRepository<UserAgree,Long> {
-}
