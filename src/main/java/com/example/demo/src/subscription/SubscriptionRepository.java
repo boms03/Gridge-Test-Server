cbd247e3e76ddb329ac6e1dcc312b0efc31a977b
boms03 <beomsuyoo@gmail.com>
2024-03-20 22:20:43 +0900

    [FEAT] 구독 확인 및 취소 컨트롤러, 서비스, 레포지토리 구현 완료

---
+++ src/main/java/com/example/demo/src/subscription/SubscriptionRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.src.subscription;
+
+import com.example.demo.common.Constant;
+import com.example.demo.src.subscription.entity.Subscription;
+import com.example.demo.src.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription,Long> {
+    Optional<Subscription>findFirstByUserIdOrderByCreatedAtDesc(Long userId);
+
+    List<Subscription> findAllBySubscriptionState(Constant.SubscriptionState subscriptionState);
+
+}
