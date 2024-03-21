00fc9ffd638a07879da2f53b9bfdf36936461321
boms03 <beomsuyoo@gmail.com>
2024-03-21 14:53:20 +0900

    [FEAT] 주문 컨트롤러 유효성 검사 및 Api 문서화 완료

--- src/main/java/com/example/demo/src/order/PurchaseController.java
+++ src/main/java/com/example/demo/src/order/PurchaseController.java
@@ -2,6 +2,8 @@ package com.example.demo.src.order;
 
 import com.example.demo.common.response.BaseResponse;
 import com.example.demo.src.order.model.PurchaseReq;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,15 +14,20 @@ public class PurchaseController {
 
     private final PurchaserService purchaserService;
 
-    @PostMapping("/order/success")
-    @ResponseBody
+    @Operation(summary = "Post 주문 정보 기록")
+    @ApiResponse(responseCode = "200", description = "주문 정보 기록 성공")
+    @ApiResponse(responseCode = "400", description = "주문 정보 기록 실패")
+    @PostMapping("/purchase/success")
     public BaseResponse<String> saveOrder(@RequestBody PurchaseReq request) {
         purchaserService.saveOrder(request);
         String response = "주문 성공 기록  성공";
         return new BaseResponse<>(response);
     }
 
-    @PostMapping("/order/fail")
+    @Operation(summary = "Post 주문 실패 정보 기록")
+    @ApiResponse(responseCode = "200", description = "주문 실패 정보 기록 성공")
+    @ApiResponse(responseCode = "400", description = "주문 실패 정보 기록 실패")
+    @PostMapping("/purchase/fail")
     @ResponseBody
     public BaseResponse<String> saveFailOrder(@RequestBody PurchaseReq request) {
         purchaserService.saveFailOrder(request);
