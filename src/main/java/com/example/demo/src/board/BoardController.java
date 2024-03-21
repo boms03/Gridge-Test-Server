e4041d55b5b16468710034002544b156a13741de
boms03 <beomsuyoo@gmail.com>
2024-03-21 14:53:34 +0900

    [FEAT] 게시글 컨트롤러 유효성 검사 및 Api 문서화 완료

--- src/main/java/com/example/demo/src/board/BoardController.java
+++ src/main/java/com/example/demo/src/board/BoardController.java
@@ -6,11 +6,15 @@ import com.example.demo.common.response.BaseResponseStatus;
 import com.example.demo.src.board.model.*;
 import com.example.demo.src.mapping.boardReport.BoardReportService;
 import com.example.demo.utils.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -23,10 +27,15 @@ public class BoardController {
     private final BoardReportService boardReportService;
     private final JwtService jwtService;
 
+    @Operation(summary = "Post 게시글 생성")
+    @ApiResponse(responseCode = "200", description = "게시글 생성 성공")
+    @ApiResponse(responseCode = "400", description = "게시글 생성 실패")
     @PostMapping("")
     public BaseResponse<String> createBoard(
-            @RequestPart(value="images") List<MultipartFile> files,
-            @RequestPart(value = "requestDto") PostBoardReq postBoardReq,
+            @Parameter(required = true, name = "이미지 및 영상 파일")
+            @RequestPart(value="files") List<MultipartFile> files,
+
+            @RequestPart(value = "requestDto") @Valid PostBoardReq postBoardReq,
             HttpServletRequest request
     ){
         if(files.size()>10){
@@ -42,8 +51,12 @@ public class BoardController {
         return new BaseResponse<>(response);
     }
 
+    @Operation(summary = "Get 게시글 조회")
+    @ApiResponse(responseCode = "200", description = "게시글 조회 성공")
+    @ApiResponse(responseCode = "400", description = "게시글 조회 실패")
     @GetMapping("")
     public BaseResponse<GetBoardRes> getBoard(
+            @Parameter(required = true, name = "게시글 아이디")
             @RequestParam Long boardId,
             HttpServletRequest request
     ){
@@ -53,8 +66,12 @@ public class BoardController {
         return new BaseResponse<>(getBoardRes);
     }
 
-    @DeleteMapping("")
+    @Operation(summary = "Put 게시글 삭제")
+    @ApiResponse(responseCode = "200", description = "게시글 삭제 성공")
+    @ApiResponse(responseCode = "400", description = "게시글 삭제 실패")
+    @PutMapping("")
     public BaseResponse<String> deleteBoard(
+            @Parameter(required = true, name = "게시글 아이디")
             @RequestParam Long boardId,
             HttpServletRequest request
     ){
@@ -64,8 +81,12 @@ public class BoardController {
         return new BaseResponse<>(response);
     }
 
+    @Operation(summary = "Get 모든 게시글 조회", description = "무한 스크롤 방식이며 마지막으로 조회한 게시글 아이디를 저장하며 조회")
+    @ApiResponse(responseCode = "200", description = "모든 게시글 조회 성공")
+    @ApiResponse(responseCode = "400", description = "모든 게시글 조회 실패")
     @GetMapping("/all")
     public BaseResponse<GetBoardsRes> getBoards(
+            @Parameter(required = false, name = "마지막으로 조회한 게시글 아이디")
             @RequestParam(required = false) Long lastBoardId,
             HttpServletRequest request
 
@@ -81,8 +102,12 @@ public class BoardController {
         return new BaseResponse<>(getBoardsRes);
     }
 
+    @Operation(summary = "Post 게시글 좋아요")
+    @ApiResponse(responseCode = "200", description = "게시글 좋아요 성공")
+    @ApiResponse(responseCode = "400", description = "게시글 좋아요 실패")
     @PostMapping("/like")
     public BaseResponse<String> likeBoard(
+            @Parameter(required = true, name = "게시글 아이디")
             @RequestParam Long boardId,
             HttpServletRequest request
     ){
@@ -91,8 +116,13 @@ public class BoardController {
         String response = "게시물 좋아요 완료";
         return new BaseResponse<>(response);
     }
+
+    @Operation(summary = "Delete 게시글 좋아요 삭제")
+    @ApiResponse(responseCode = "200", description = "게시글 좋아요 삭제 성공")
+    @ApiResponse(responseCode = "400", description = "게시글 좋아요 삭제 실패")
     @DeleteMapping("/like")
     public BaseResponse<String> unlikeBoard(
+            @Parameter(required = true, name = "게시글 아이디")
             @RequestParam Long boardId,
             HttpServletRequest request
     ){
@@ -102,9 +132,15 @@ public class BoardController {
         return new BaseResponse<>(response);
     }
 
+    @Operation(summary = "Post 게시글 신고")
+    @ApiResponse(responseCode = "200", description = "게시글 신고")
+    @ApiResponse(responseCode = "400", description = "게시글 신고")
     @PostMapping("/report")
     public BaseResponse<String> reportBoard(
+            @Parameter(required = true, name = "게시글 아이디")
             @RequestParam Long boardId,
+
+            @Parameter(required = true, name = "신고 카테고리 아이디", description = "(아이디) 1. 스팸, 2. 나체 이미지 또는 성적 행위, 3. 혐오 발언 또는 상징, 4. 폭력 또는 위험한 단체, 5. 불법 또는 규제 상품 판매, 6. 따돌림 또는 괴롭힘, 7. 지식재산권 침해, 8. 자살 또는 자해, 9. 섭식 장애, 10. 사기 또는 거짓, 11. 거짓 정보, 12. 마음에 들지 않습니다")
             @RequestParam Long reportId,
             HttpServletRequest request
     ){
