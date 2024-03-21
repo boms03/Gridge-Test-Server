b868fbdded93d7a33f43bc1a3c8a1bccde63ec64
boms03 <beomsuyoo@gmail.com>
2024-03-21 14:53:08 +0900

    [FEAT] 구독 컨트롤러 유효성 검사 및 Api 문서화 완료

--- src/main/java/com/example/demo/src/subscription/SubscriptionController.java
+++ src/main/java/com/example/demo/src/subscription/SubscriptionController.java
@@ -2,6 +2,8 @@ package com.example.demo.src.subscription;
 
 import com.example.demo.common.response.BaseResponse;
 import com.example.demo.utils.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +19,9 @@ public class SubscriptionController {
     private final JwtService jwtService;
     private final SubscriptionService subscriptionService;
 
+    @Operation(summary = "Get 구독 체크")
+    @ApiResponse(responseCode = "200", description = "구독 체크 성공")
+    @ApiResponse(responseCode = "400", description = "구독 체크 실패")
     @GetMapping("")
     public BaseResponse<String> subscribe(
             HttpServletRequest request
@@ -26,7 +31,11 @@ public class SubscriptionController {
         String response = "구독 여부 체크 완료";
         return new BaseResponse<>(response);
     }
-    @DeleteMapping("")
+
+    @Operation(summary = "Put 구독 취소")
+    @ApiResponse(responseCode = "200", description = "구독 취소 성공")
+    @ApiResponse(responseCode = "400", description = "구독 취소 실패")
+    @PutMapping("")
     public BaseResponse<String> unsubscribe(
             HttpServletRequest request
     ){
