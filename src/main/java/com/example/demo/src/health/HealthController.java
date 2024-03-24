78b0693bcd1c87c404734018241672a58c09074c
boms03 <beomsuyoo@gmail.com>
2024-03-24 19:22:55 +0900

    [DOCS] 헬스체크 API 명세서 수정 및 추가 완료

--- src/main/java/com/example/demo/src/health/HealthController.java
+++ src/main/java/com/example/demo/src/health/HealthController.java
@@ -1,10 +1,18 @@
 package com.example.demo.src.health;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+
+    /**
+     * AWS Elasticbeanstalk health 체크용 API
+     * [GET] /health
+     * @return String
+     */
+    @Operation(summary = "Get AWS Elasticbeanstalk health 체크")
     @GetMapping("/health")
     public String health(){
         return "healthChecked";
